0b426763f0da78d21b52afcc451345e614e3edc8
Andrew Quach <andrewrquach@gmail.com>
2022-03-15 19:08:21 -0700

    ✨ Added share button and renamed css class selectors

--- js/main.js
+++ js/main.js
@@ -1,68 +1,82 @@
-/** 
- *  Constants
- */
+//addPlayerBtn give 3 word names
+
+/*
+ ██████╗ ██████╗ ███╗   ██╗███████╗████████╗ █████╗ ███╗   ██╗████████╗███████╗
+██╔════╝██╔═══██╗████╗  ██║██╔════╝╚══██╔══╝██╔══██╗████╗  ██║╚══██╔══╝██╔════╝
+██║     ██║   ██║██╔██╗ ██║███████╗   ██║   ███████║██╔██╗ ██║   ██║   ███████╗
+██║     ██║   ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║╚██╗██║   ██║   ╚════██║
+╚██████╗╚██████╔╝██║ ╚████║███████║   ██║   ██║  ██║██║ ╚████║   ██║   ███████║
+ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝                                                                              
+*/
+
 const addPlayerBtn = document.querySelector("#add-btn");
 const inputtedPlayerName = document.querySelector("#inputted-player-name");
-const addPlayerForm = document.querySelector("#player-form");
-const addedPlayers = document.querySelectorAll("ul#players-list>li");
+const playerForm = document.querySelector(".main__form");
 const playersList = document.querySelector("#players-list");
-const errorMsg = document.querySelector("#error-msg");
-const deletePlayerBtn = document.querySelector("#delete-btn");
-const clearPlayersBtn = document.querySelector("#clear-btn");
-const randomizePlayersBtn = document.querySelector("#rando-btn");
+const addErrorMsg = document.querySelector("#add-error-msg");
+const randoErrorMsg = document.querySelector("#rando-error-msg");
+const deleteBtn = document.querySelector("#delete-btn");
+const clearBtn = document.querySelector("#clear-btn");
+const randoBtn = document.querySelector("#rando-btn");
 const teamOneList = document.querySelector("#team-one");
 const teamTwoList = document.querySelector("#team-two");
+let players = document.querySelectorAll("ul#players-list>li");
+let playersInTeams;
 
-/** 
- *  Function expressions
- */
+let teams;
+let draggablePlayer;
+
+/*
+███████╗██╗   ██╗███╗   ██╗ ██████╗████████╗██╗ ██████╗ ███╗   ██╗    ███████╗██╗  ██╗██████╗ ██████╗ ███████╗███████╗███████╗██╗ ██████╗ ███╗   ██╗███████╗
+██╔════╝██║   ██║████╗  ██║██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║    ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝██║██╔═══██╗████╗  ██║██╔════╝
+█████╗  ██║   ██║██╔██╗ ██║██║        ██║   ██║██║   ██║██╔██╗ ██║    █████╗   ╚███╔╝ ██████╔╝██████╔╝█████╗  ███████╗███████╗██║██║   ██║██╔██╗ ██║███████╗
+██╔══╝  ██║   ██║██║╚██╗██║██║        ██║   ██║██║   ██║██║╚██╗██║    ██╔══╝   ██╔██╗ ██╔═══╝ ██╔══██╗██╔══╝  ╚════██║╚════██║██║██║   ██║██║╚██╗██║╚════██║
+██║     ╚██████╔╝██║ ╚████║╚██████╗   ██║   ██║╚██████╔╝██║ ╚████║    ███████╗██╔╝ ██╗██║     ██║  ██║███████╗███████║███████║██║╚██████╔╝██║ ╚████║███████║
+╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝    ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝                                                                                                                                                           
+*/
+
+const toggleActiveState = function() {
+    this.classList.contains("active") ?
+        this.classList.remove("active") : this.classList.add("active");
+};
+const updatePlayersList = () => players = document.querySelectorAll("ul#players-list>li");
 const addPlayer = function() {
     const inputtedPlayerNameValue = inputtedPlayerName.value;
 
     if (doesPlayerExist(inputtedPlayerNameValue)) {
-        createErrorMsg("Player added already!", 1500);
+        createErrorMsg("Player added already!", 1500, addErrorMsg);
     } else if (
         inputtedPlayerNameValue.value === 0 ||
         inputtedPlayerNameValue === ""
     ) {
-        createErrorMsg("The field is empty!", 1500);
+        createErrorMsg("The field is empty!", 1500, addErrorMsg);
     } else {
         let newPlayer = document.createElement("li");
-        newPlayer.classList.add("player", "ripple");
-        newPlayer.addEventListener("click", function() {
-            if (this.classList.contains("active")) {
-                this.classList.remove("active");
-            } else {
-                this.classList.add("active");
-            }
-        });
+        newPlayer.classList.add("player", "--ripple");
+        newPlayer.addEventListener("click", toggleActiveState);
         newPlayer.textContent = inputtedPlayerNameValue;
         playersList.appendChild(newPlayer);
-        addPlayerForm.reset();
-        isListEmpty();
+        playerForm.reset();
+        updatePlayersList();
+        removeListLine();
     }
 };
-const doesPlayerExist = (newPlayer) => [...addedPlayers].map((player) => player.innerText).includes(newPlayer);
-const createErrorMsg = function(msg, duration) {
-    errorMsg.innerHTML = msg;
-    errorMsg.style.display = "block";
+const doesPlayerExist = (newPlayer) => [...players].map((player) => player.innerText).includes(newPlayer);
+const createErrorMsg = function(msg, duration, ele) {
+    ele.innerHTML = msg;
+    ele.style.display = "block";
     setTimeout(function() {
-        errorMsg.style.display = "none";
+        ele.style.display = "none";
     }, duration);
 };
 const remove = (sel) =>
     document.querySelectorAll(sel).forEach((el) => el.remove());
-const isListEmpty = () =>
-    document.querySelectorAll("#players-list>li").length === 0 ?
+const isListEmpty = (selector) =>
+    document.querySelectorAll(selector).length === 0;
+const removeListLine = () =>
+    isListEmpty("#players-list>li") ?
     (playersList.style.display = "none") :
     (playersList.style.display = "block");
-const randomlyFillTeam = (randomPlayers, team) =>
-    randomPlayers.forEach((playerName) => {
-        const ele = document.createElement("li");
-        ele.classList.add("team-player");
-        ele.textContent = playerName;
-        team.appendChild(ele);
-    });
 const randomize = function(players) {
     let playersArray = [...players].map((node) => node.innerText);
     let currentIndex = playersArray.length,
@@ -80,6 +94,15 @@ const randomize = function(players) {
 
     return playersArray;
 };
+const randomlyFillTeam = (randomPlayers, team) =>
+    randomPlayers.forEach((playerName) => {
+        const ele = document.createElement("li");
+        ele.classList.add("player");
+        ele.setAttribute("draggable", true);
+        ele.textContent = playerName;
+        team.appendChild(ele);
+        playersInTeams = document.querySelectorAll(".player");
+    });
 const assignTeams = function(players) {
     const halfLength = Math.ceil(players.length / 2);
     const teamOne = players.splice(0, halfLength);
@@ -96,30 +119,61 @@ const assignTeams = function(players) {
         randomlyFillTeam(teamTwo, teamOneList);
         randomlyFillTeam(teamOne, teamTwoList);
     }
+
+    teams = document.querySelector("h3+ul");
+    makeDraggable();
+    makeDroppable();
 };
-const randomizeAndAssign = () =>
+const randomizeAndAssign = () => isListEmpty("#players-list>li") ? createErrorMsg("There are no players to randomize!", 1500, randoErrorMsg) :
     assignTeams(randomize(document.querySelectorAll("#players-list>li")));
-/** 
- *  Event listeners
- */
-addPlayerBtn.addEventListener("click", addPlayer);
-deletePlayerBtn.addEventListener("click", function() {
-    remove(".active");
-    isListEmpty();
-});
-clearPlayersBtn.addEventListener("click", function() {
-    remove("li.player");
-    isListEmpty();
-});
-randomizePlayersBtn.addEventListener("click", randomizeAndAssign);
 
-
-addedPlayers.forEach((player) => {
-    player.addEventListener("click", function() {
-        if (this.classList.contains("active")) {
-            this.classList.remove("active");
-        } else {
-            this.classList.add("active");
-        }
+const makeDraggable = () => {
+    playersInTeams.forEach((player) => {
+        player.addEventListener("dragstart", dragStart);
+        player.addEventListener("dragend", dragEnd);
     });
+};
+const makeDroppable = () => {
+    teams.forEach((team) => {
+        team.addEventListener("dragover", dragOver);
+        team.addEventListener("dragenter", dragEnter);
+        team.addEventListener("dragleave", dragLeave);
+        team.addEventListener("drop", dragDrop);
+    })
+};
+const dragStart = () => draggablePlayer = this;
+const dragEnd = () => draggablePlayer = null;
+const dragOver = (e) => e.preventDefault();
+const dragEnter = () => console.log("dragEnter");
+const dragLeave = () => console.log("dragLeave");
+const dragDrop = () => {
+    this.appendChild(draggablePlayer);
+};
+/*
+███████╗██╗   ██╗███████╗███╗   ██╗████████╗    ██╗     ██╗███████╗████████╗███████╗███╗   ██╗███████╗██████╗ ███████╗
+██╔════╝██║   ██║██╔════╝████╗  ██║╚══██╔══╝    ██║     ██║██╔════╝╚══██╔══╝██╔════╝████╗  ██║██╔════╝██╔══██╗██╔════╝
+█████╗  ██║   ██║█████╗  ██╔██╗ ██║   ██║       ██║     ██║███████╗   ██║   █████╗  ██╔██╗ ██║█████╗  ██████╔╝███████╗
+██╔══╝  ╚██╗ ██╔╝██╔══╝  ██║╚██╗██║   ██║       ██║     ██║╚════██║   ██║   ██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗╚════██║
+███████╗ ╚████╔╝ ███████╗██║ ╚████║   ██║       ███████╗██║███████║   ██║   ███████╗██║ ╚████║███████╗██║  ██║███████║
+╚══════╝  ╚═══╝  ╚══════╝╚═╝  ╚═══╝   ╚═╝       ╚══════╝╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚══════╝                                                                                                                      
+*/
+
+addPlayerBtn.addEventListener("click", addPlayer);
+deleteBtn.addEventListener("click", function() {
+    remove(".active");
+    updatePlayersList();
+    isListEmpty("#players-list");
+});
+clearBtn.addEventListener("click", function() {
+    remove("li.player");
+    isListEmpty("#players-list");
+    updatePlayersList();
+    removeListLine();
+});
+randoBtn.addEventListener("click", randomizeAndAssign);
+players
+
+//FOR TESTING! Adds players on startup (The Joeys)
+players.forEach((player) => {
+    player.addEventListener("click", toggleActiveState);
 });
